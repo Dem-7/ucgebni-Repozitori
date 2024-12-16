0fc35529cd6dad722b1cf7c48ca372c9869ec93d
Dem-7 <dem67713@gmail.com>
2024-12-16 15:30:37 +0300

    vb

--- lml.py
+++ lml.py
@@ -1,5 +1,38 @@
-def calc(a, b):
-    print(a + b)
-    print(a - b)
-    print(a * b)
-    print(a/b)
+class Hero:
+    def __init__(self, name, health=100, attack_power=20):
+        self.name = name
+        self.health = health
+        self.attack_power = attack_power
+
+    def attack(self, other):
+        print(f"{self.name} атаковал {other.name}")
+        other.health -= self.attack_power
+        print(f"У {other.name} осталось {other.health} здоровья")
+
+    def is_alive(self):
+        return self.health > 0
+
+
+class Game:
+    def __init__(self, player, computer):
+        self.player = player
+        self.computer = computer
+
+    def start(self):
+        while self.player.is_alive() and self.computer.is_alive():
+            self.player.attack(self.computer)
+            if not self.computer.is_alive():
+                print(f"{self.player.name} выиграл!")
+                break
+
+            self.computer.attack(self.player)
+            if not self.player.is_alive():
+                print(f"{self.computer.name} выиграл!")
+
+
+# Создание экземпляров и запуск игры
+player = Hero("Человек", 100, 20)
+computer = Hero("Компьютер", 100, 20)
+game = Game(player, computer)
+game.start()
+
