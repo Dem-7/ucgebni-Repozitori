c82fca587f775e70ab6f299d9d6aeea34e17ca2d
Dem-7 <dem67713@gmail.com>
2024-12-16 15:25:26 +0300

    ls

--- lml.py
+++ lml.py
@@ -1,7 +1,5 @@
-from random import randrange
-dd = randrange(1, 83, 3)
-print(dd)
-if dd % 2 == 0:
-    print("Выбрано чётное число ")
-else:
-    print("Выбранное число нечётное")
+def calc(a, b):
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    print(a/b)
